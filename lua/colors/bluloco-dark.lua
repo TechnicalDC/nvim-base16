a196448700cdd355f7dd0667e7cc12817112699e
Dilip Chauhan <chauhandilip305@gmail.com>
2024-03-31 16:19:40 +0530

    first commit

---
+++ lua/colors/bluloco-dark.lua
@@ -0,0 +1,8 @@
+return {
+   base00 = '#41444d', base01 = '#fc2f52', base02 = '#25a45c', base03 = '#ff936a',
+   base04 = '#3476ff', base05 = '#7a82da', base06 = '#4483aa', base07 = '#cdd4e0',
+   base08 = '#8f9aae', base09 = '#ff6480', base0A = '#3fc56b', base0B = '#f9c859',
+   base0C = '#10b1fe', base0D = '#ff78f8', base0E = '#5fb9bc', base0F = '#ffffff'
+}
+
+
